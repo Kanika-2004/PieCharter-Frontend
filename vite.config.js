e3df301639d09dff847eb54bb5cd5dc257449682
Kanika-2004 <155616397+Kanika-2004@users.noreply.github.com>
2024-08-15 17:06:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/kanika-2004.github.io/PieCharter-Frontend',
+  // base:'/kanika-2004.github.io/PieCharter-Frontend',
   plugins: [react()],
 })
 
